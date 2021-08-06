6a71ef0f809ad5cc08d4f84df7a72c460fd48ebc
Rhonda Warner <sunflair_2000@yahoo.com>
2021-08-06 00:40:21 -0500

    Firebase Auth for signup complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
+import Signup from './components/Signup';
+import { Container } from "react-bootstrap"
+import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
-const [count, setCount] =useState(0)
-
-const increment = ()=> {
-setCount(count +1)
-}
-
-const decrement = ()=> {
-setCount(count -1)
-}
 
   return (
-<>
-  <p>{count}</p>
-<button onClick={increment}>Increment</button>
-<button onClick={decrement}>Decrement</button>
-</>
+    <AuthProvider>
+      <Container className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh", backgroundColor: "#cacacc" }}>
+        <div className="w-100" style={{ maxWidth: "400px", backgroundColor: "hsl(323, 85%, 34%" }}>
+          <Signup />
+        </div>
+      </Container>
+    </AuthProvider>
+
   );
 }
 
