872ff1dc453966b0cdc8c271ab0f8431a5e9d75c
Rhonda Warner <sunflair_2000@yahoo.com>
2021-08-06 21:27:03 -0500

    will convert to Context to correct rendering error

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,13 @@
 import React, { useRef, useState } from 'react'
 import { Card, Form, Button, Alert } from 'react-bootstrap'
-import { useAuth } from '../contexts/AuthContext'
+import { useContext } from '../contexts/AuthContext'
 import { Link, useHistory } from 'react-router-dom'
 
 
 export default function Login() {
 	const emailRef = useRef()
 	const passwordRef = useRef()
-	const { login } = useAuth()
+	const { login } = useContext()
 	const [error, setError] = useState('')
 	const [loading, setLoading] = useState(false)
 	const history = useHistory()
