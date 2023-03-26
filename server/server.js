72b478cbda5dea5170268d44cec08eb11c3ef6a1
Sunflair0 <62845098+Sunflair0@users.noreply.github.com>
2023-03-25 21:42:20 -0500

    favicon update

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 
 app.use("^/$", (req, res, next) => {
-	fs.readFile(path.resolve("./build/index.html"), "utf - 8", (err, data) => {
+	fs.readFile(path.resolve("./build/index.html"), (err, data) => {
 		if (err) {
 			console.log(err)
 			return res.status(500).send("An Error Occurred")
